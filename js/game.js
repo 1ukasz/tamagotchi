ef8aa8888ed503f02c7e1f550f18d8317a5a4d83
lukaszmiszczak <lukasmiszczak@gmail.com>
2023-02-27 18:55:47 +0100

    life-parameters-implementation

--- js/game.js
+++ js/game.js
@@ -5,7 +5,12 @@ export default class Game {
     this.tamagotchi = new Tamagotchi();
   }
 
-  start = ({ healthElement, hungerElement, energyElement, funElement }) => {
+  start = ({
+    healthElement,
+    hungerElement,
+    energyElement,
+    funElement
+  }) => {
     this.tamagotchi.mount({
       healthElement,
       hungerElement,
@@ -14,5 +19,61 @@ export default class Game {
     });
 
     console.log("Game started");
+
+    const decreaseHealth = () => {
+      if (this.tamagotchi.hunger.value <= 0) this.tamagotchi.health.value--;
+      this.tamagotchi.mount({
+        healthElement,
+        hungerElement,
+        energyElement,
+        funElement,
+      })
+      
+      if (this.tamagotchi.health.value <= 0) {
+        clearInterval(healthId);
+        clearInterval(hungerId);
+        clearInterval(energyId);
+        clearInterval(funId);
+        console.log("You lost")
+      }
+    };
+
+    const decreaseHunger = () => {
+      this.tamagotchi.hunger.value--;
+      this.tamagotchi.mount({
+        healthElement,
+        hungerElement,
+        energyElement,
+        funElement,
+      })
+    };
+
+    const decreaseEnergy = () => {
+      this.tamagotchi.energy.value--;
+      if (this.tamagotchi.fun.value <= 0) this.tamagotchi.energy.value--;
+      this.tamagotchi.mount({
+        healthElement,
+        hungerElement,
+        energyElement,
+        funElement,
+      })
+    };
+
+    const decreaseFun = () => {
+      this.tamagotchi.fun.value--;
+      this.tamagotchi.mount({
+        healthElement,
+        hungerElement,
+        energyElement,
+        funElement,
+      })
+    };
+
+    let healthId = setInterval(decreaseHealth, 1000);
+    let hungerId = setInterval(decreaseHunger, 1000);
+    let energyId = setInterval(decreaseEnergy, 2000);
+    let funId = setInterval(decreaseFun, 1000);
+
   };
+
 }
--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -1,9 +1,9 @@
 export default class Tamagotchi {
   constructor() {
-    this.health = { value: 4, importance: 1 };
-    this.hunger = { value: 6, importance: 3 };
-    this.energy = { value: 9, importance: 2 };
-    this.fun = { value: 4, importance: 4 };
+    this.health = { value: 10, importance: 1 };
+    this.hunger = { value: 10, importance: 3 };
+    this.energy = { value: 10, importance: 2 };
+    this.fun = { value: 10, importance: 4 };
     console.log("Tamagotchi initialized");
   }
 
