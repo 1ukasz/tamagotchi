32a66ee3ca97ad26c721456114351d8aa03a0ced
Lukasz <lukaszmiszczak92@gmail.com>
2023-02-27 21:18:31 +0100

    life-parameters-implementation

--- js/game.js
+++ js/game.js
@@ -5,12 +5,7 @@ export default class Game {
     this.tamagotchi = new Tamagotchi();
   }
 
-  start = ({
-    healthElement,
-    hungerElement,
-    energyElement,
-    funElement
-  }) => {
+  start = ({ healthElement, hungerElement, energyElement, funElement }) => {
     this.tamagotchi.mount({
       healthElement,
       hungerElement,
@@ -21,59 +16,63 @@ export default class Game {
     console.log("Game started");
 
     const decreaseHealth = () => {
-      if (this.tamagotchi.hunger.value <= 0) this.tamagotchi.health.value--;
+      if (
+        this.tamagotchi.hunger.value <= 0 ||
+        this.tamagotchi.energy.value <= 0
+      )
+        this.tamagotchi.health.value--;
       this.tamagotchi.mount({
         healthElement,
         hungerElement,
         energyElement,
         funElement,
-      })
-      
+      });
+
       if (this.tamagotchi.health.value <= 0) {
         clearInterval(healthId);
         clearInterval(hungerId);
         clearInterval(energyId);
         clearInterval(funId);
-        console.log("You lost")
+        console.log("You lost");
       }
     };
 
     const decreaseHunger = () => {
-      this.tamagotchi.hunger.value--;
+      if (this.tamagotchi.hunger.value > 0) this.tamagotchi.hunger.value--;
       this.tamagotchi.mount({
         healthElement,
         hungerElement,
         energyElement,
         funElement,
-      })
+      });
     };
 
     const decreaseEnergy = () => {
-      this.tamagotchi.energy.value--;
-      if (this.tamagotchi.fun.value <= 0) this.tamagotchi.energy.value--;
+      if (this.tamagotchi.energy.value > 0) {
+        this.tamagotchi.energy.value--;
+        if (this.tamagotchi.fun.value <= 0) this.tamagotchi.energy.value--;
+      }
       this.tamagotchi.mount({
         healthElement,
         hungerElement,
         energyElement,
         funElement,
-      })
+      });
     };
 
     const decreaseFun = () => {
-      this.tamagotchi.fun.value--;
+      if (this.tamagotchi.fun.value > 0) this.tamagotchi.fun.value--;
       this.tamagotchi.mount({
         healthElement,
         hungerElement,
         energyElement,
         funElement,
-      })
+      });
     };
 
     let healthId = setInterval(decreaseHealth, 1000);
     let hungerId = setInterval(decreaseHunger, 1000);
     let energyId = setInterval(decreaseEnergy, 2000);
     let funId = setInterval(decreaseFun, 1000);
-
   };
-
 }
